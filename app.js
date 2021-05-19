5c3de157f6dd2b38b31600972a166402b15b559d
lettuce <lettuce.lu@lettucelu.com>
2021-05-20 00:34:27 +0800

    add result.handlebars

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -15,7 +15,9 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.render('result')
+  // res.send(req.body)
 })
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
 })
