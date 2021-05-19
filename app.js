306cbbc35c2c1d90b101aa0de408cc09a0209d99
lettuce <lettuce.lu@lettucelu.com>
2021-05-19 20:01:00 +0800

    add views folder & static file

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
-
 const PORT = 3000
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-  res.send('Shortner')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
 })
+
