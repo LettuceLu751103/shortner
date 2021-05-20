fb30a0ac58880be63b301c7e775bdd40b3a5e347
lettuce <lettuce.lu@lettucelu.com>
2021-05-20 21:25:32 +0800

    add getKey function

--- app.js
+++ app.js
@@ -14,7 +14,30 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  res.render('result')
+  const urlText = req.body.urlText
+  console.log(urlText)
+
+  function getKey() {
+    const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+    const upperCase = lowerCase.toUpperCase()
+    const number = '0123456789'
+    const lowerCaseArray = lowerCase.split('')
+    const upperCaseArray = upperCase.split('')
+    const numberArray = number.split('')
+    const englishArray = lowerCaseArray.concat(upperCaseArray)
+    let newArray = englishArray.concat(numberArray)
+    let newWords = ''
+    for (let i = 0; i < 6; i++) {
+      const newIndex = Math.floor(Math.random(newArray.length) * newArray.length)
+      newWords += newArray[newIndex]
+    }
+    return newWords
+  }
+
+  console.log('key:', getKey())
+  console.log('value:', urlText)
+  // 查詢資料庫有沒有相同的資料, 若是有則重新
+  res.render('result', { urlText: urlText, randomNum: getKey() })
   // res.send(req.body)
 })
 
