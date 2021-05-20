5e2ffaccb3122199460d0549e4bed70639d99452
lettuce <lettuce.lu@lettucelu.com>
2021-05-20 21:27:55 +0800

    install mongoose & require

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
+// 資料庫設定
+const mongoose = require('mongoose') // 載入 mongoose
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+
+
+mongoose.connect('mongodb://localhost/urlDB', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+
+
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -15,7 +35,6 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const urlText = req.body.urlText
-  console.log(urlText)
 
   function getKey() {
     const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
